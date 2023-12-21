eb4e4e5aae94d225e7ff0ca64ceb963390612d15
Danie Roux <danie@danieroux.com>
2023-12-21 14:48:45 +0200

    Make `dev` work and make `build` go to `dist` directory
    - Copy the manifest to the right locations for dev and production
    - Explaining how to get to the hot-reload plugin
    - Ignore generated main.js for development
    - Ignore build directories

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,65 @@
-import typescript from '@rollup/plugin-typescript';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import copy from "rollup-plugin-copy";
+
+const DEV_PLUGIN_LOCATION =
+  "docs/test-vault/.obsidian/plugins/flashcards-obsidian/";
 
 const PRODUCTION_PLUGIN_CONFIG = {
-  input: 'main.ts',
+  input: "main.ts",
   output: {
-    dir: '.',
-    sourcemap: 'inline',
+    dir: "dist",
+    sourcemap: "inline",
     sourcemapExcludeSources: true,
-    format: 'cjs',
-    exports: 'default'
+    format: "cjs",
+    exports: "default",
   },
-  external: ['obsidian'],
+  external: ["obsidian"],
   plugins: [
     typescript(),
-    nodeResolve({browser: true}),
+    nodeResolve({ browser: true }),
     commonjs(),
-  ]
+    copy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: "dist",
+        },
+      ],
+    }),
+  ],
 };
 
 const DEV_PLUGIN_CONFIG = {
-  input: 'main.ts',
+  input: "main.ts",
   output: {
-    dir: 'docs/test-vault/.obsidian/plugins/flashcards-obsidian/',
-    sourcemap: 'inline',
-    format: 'cjs',
-    exports: 'default'
+    dir: DEV_PLUGIN_LOCATION,
+    sourcemap: "inline",
+    format: "cjs",
+    exports: "default",
   },
-  external: ['obsidian'],
+  external: ["obsidian"],
   plugins: [
     typescript(),
-    nodeResolve({browser: true}),
+    nodeResolve({ browser: true }),
     commonjs(),
-  ]
+    copy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: DEV_PLUGIN_LOCATION,
+        },
+      ],
+    }),
+  ],
 };
 
-let configs = []
+let configs = [];
 
 if (process.env.BUILD === "dev") {
   configs.push(DEV_PLUGIN_CONFIG);
-} else if (process.env.BUILD === "production" ) {
+} else if (process.env.BUILD === "production") {
   configs.push(PRODUCTION_PLUGIN_CONFIG);
 } else {
   configs.push(DEV_PLUGIN_CONFIG);
