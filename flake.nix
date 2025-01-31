80b4238cd1a32d9466f864a04575cf4dfbefd422
Danie Roux <danie@danieroux.com>
2025-02-01 00:09:55 +0200

    Upgrade to later Anki and Anki Connect

--- flake.nix
+++ flake.nix
@@ -1,49 +1,26 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  outputs = { self, nixpkgs, flake-utils } @ inputs:
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        fo-dev = pkgs.writeShellScriptBin "fo-dev" "npm install && npm run bootstrap && npm run dev";
+        fo-test = pkgs.writeShellScriptBin "fo-test" "npm install && npm run test";
+        fo-reset-test-environment = pkgs.writeShellScriptBin "fo-reset-test-environment" "cd $(git rev-parse --show-toplevel) && git checkout -- docs/test-vault/ tests/anki_base && git clean -fd tests/anki_base/flashcards-obsidian";
+        fo-build = pkgs.writeShellScriptBin "fo-build" "npm install && npm run build";
+        fo-anki-for-testing = pkgs.writeShellScriptBin "fo-anki-for-testing" "${pkgs.anki-bin.outPath}/Applications/Anki.app/Contents/MacOS/anki --base $(git rev-parse --show-toplevel)/tests/anki_base";
+      in
+      rec {
+        formatter = pkgs.nixpkgs-fmt;
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = [
-                    pkgs.nodejs
-                    pkgs.anki-bin # https://nixos.wiki/wiki/Anki
-                  ];
-
-                  # fo = flashcards-obsidian
-                  scripts.fo-dev.exec = "npm install && npm run bootstrap && npm run dev";
-                  scripts.fo-test.exec = "npm install && npm run test";
-                  scripts.fo-reset-test-environment.exec = "cd $(git rev-parse --show-toplevel) && git checkout -- docs/test-vault/ tests/anki_base && git clean -fd tests/anki_base/flashcards-obsidian";
-                  scripts.fo-build.exec = "npm install && npm run build";
-                  # Assumes OSX only (for now)
-                  scripts.fo-anki-for-testing.exec = "${pkgs.anki-bin.outPath}/Applications/Anki.app/Contents/MacOS/anki --base $(git rev-parse --show-toplevel)/tests/anki_base";
-
-                  enterShell = ''
+        devShells.default = pkgs.mkShellNoCC {
+          shellHook = ''
+                    echo "Open the vault in tests/obsidian_vault"
                     echo
                     echo "Run the scripts that are available in this environment:"
                     echo
@@ -57,11 +34,17 @@
                     echo fo-reset-test-environment
                     echo
                     echo "fo-<tab> for more"
-                  '';
+          '';
 
-                }
-              ];
-            };
-          });
-    };
+          packages = [ 
+            pkgs.nodejs 
+            pkgs.anki-bin 
+            fo-dev
+            fo-test
+            fo-build 
+            fo-reset-test-environment
+            fo-anki-for-testing
+          ];
+        };
+      });
 }
